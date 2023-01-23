a051f081c2fd98e41943bb965e66063de0316a36
samAricha <samaricha001@gmail.com>
2023-01-24 00:47:16 +0300

    setting header and wrapper

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+
+</script>
+
+
+<template>
+  <header class="flex items-center justify-between p-4 b-white shadow-lg">
+    <h1 class="text-xl">
+      Fast <span class="text-[#4ad97d] ">Post</span>
+    </h1>
+    <button class="bg-[#4ad97d] p-2 rounded-md text-white">Create Poll</button>
+    <div class="flex items-center justify-evenly space-x-3">
+      <button class="text-red-500">Public Polls</button>
+      <div class="space-x-2 border-l border-gray-300 px-3">
+        <button class="text-gray-600">Login</button>
+        <button class="bg-[#4097fe] text-white p-2 rounded-md">Signup</button>
+      </div>
+    </div>
+
+  </header>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Header from "@/components/Header.vue";
+import Wrapper from "@/components/Wrapper.vue";
+</script>
+
+<template>
+    <Header/>
+    <Wrapper/>
+
+</template>
